203095f63d799a3f9edca9b96b092d02d42eba7d
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-03 20:53:49 -0500

    Added new DbContext

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -4,5 +4,9 @@ namespace DotNet_Login_Reg.Models
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }  
+
     }
 }
