e0f362cb081c8185ef9e51194f28732bcd090757
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-03 20:43:24 -0500

    Adding User class

--- Models/User.cs
+++ Models/User.cs
@@ -7,19 +7,26 @@ namespace DotNet_Login_Reg.Models
     {
         [Key]
         public int UserId { get; set; }
+
         [Required]
         public string FirstName { get; set; }
+
         [Required]
         public string LastName { get; set; }
+
         [EmailAddress]
         [Required]
         public string Email { get; set; }
+
         [DataType(DataType.Password)]
         [Required]
         [MinLength(8, ErrorMessage = "Password must be 8 characters or longer!")]
         public string Password { get; set; }
+
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
         // Will not be mapped to your users table!
         [NotMapped]
         [Compare("Password")]
