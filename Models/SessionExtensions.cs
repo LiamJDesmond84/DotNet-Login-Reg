b8b285a9456b6c37e9e032ef34f71277c087d837
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-07 18:21:38 -0500

    Adding SessionExtensions model

--- Models/SessionExtensions.cs
+++ Models/SessionExtensions.cs
@@ -1,6 +1,24 @@
-﻿namespace DotNet_Login_Reg.Models
+﻿using System.Text.Json;
+
+namespace DotNet_Login_Reg.Models
 {
     public class SessionExtensions
     {
+
+        // We can call ".SetObjectAsJson" just like our other session set methods, by passing a key and a value
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            // This helper function simply serializes the object to JSON and stores it as a string in session
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        // generic type T is a stand-in indicating that we need to specify the type on retrieval
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            string value = session.GetString(key);
+            // Upon retrieval the object is deserialized based on the type we specified
+            return value == null ? default(T) : JsonSerializer.Deserialize<T>(value);
+        }
+
     }
 }
